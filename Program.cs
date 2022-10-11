b48b77e0645cdec92421e4a9c250ffaead1e82b0
Denis <epselent33@gmail.com>
2022-10-11 13:56:50 +0500

    добавил метод заполнения массива и вывода в консоль.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//метод заполнения массива и вывода в консоль.
+
+int[] FillRandomArray(int _l, int _start = 10, int _end = 100)
+{
+	int[] array = new int[_l];
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = new Random().Next(_start, _end);
+	}
+	return array;
+}
+void PrintArray(int[] arr)
+{
+	Console.Write("[");
+	foreach (var item in arr)
+	{
+		Console.Write(item + " ");
+	}
+	Console.Write("]");
+}
