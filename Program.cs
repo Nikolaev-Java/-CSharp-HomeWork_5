c78d1c30444a1aa022882acf173dbe0f31dcd4e3
Denis <epselent33@gmail.com>
2022-10-11 14:25:31 +0500

    дополнил методами для double массива

--- Program.cs
+++ Program.cs
@@ -9,12 +9,30 @@ int[] FillRandomArray(int _l, int _start = 10, int _end = 100)
 	}
 	return array;
 }
+double[] FillRandomArrayReal(int _l)
+{
+	double[] array = new double[_l];
+	for (int i = 0; i < array.Length; i++)
+	{
+		array[i] = Math.Round((new Random().NextDouble() * 100), 2);
+	}
+	return array;
+}
 void PrintArray(int[] arr)
 {
 	Console.Write("[");
 	foreach (var item in arr)
 	{
 		Console.Write(item + " ");
+	}
+	Console.Write("]");
+}
+void PrintArrayReal(double[] arr)
+{
+	Console.Write("[");
+	foreach (var item in arr)
+	{
+		Console.Write(item + " | ");
 	}
 	Console.Write("]");
 }
